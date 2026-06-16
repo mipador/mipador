52a7195e919d334d9b6442bc48b364c9aed431eb
anas <anasanasri3030@gmail.com>
2026-06-16 13:30:31 +0100

    Updates

---
+++ assets/rotate-ccw-DXUenY0K.js
@@ -0,0 +1,1 @@
+import{c as t}from"./index-MyYuTCTo.js";const o=[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]],c=t("arrow-left",o);const e=[["path",{d:"M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8",key:"1357e3"}],["path",{d:"M3 3v5h5",key:"1xhq8a"}]],r=t("rotate-ccw",e);export{c as A,r as R};
